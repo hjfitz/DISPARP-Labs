d9ff34c3aef8b41c1ce3caa9741eaedc9ecff0a4
up780461 <up780461@1.capita.vpn.port.ac.uk>
2019-01-28 11:16:56 +0000

    rename

--- src/week2/MandelbrotQuadrant.java
+++ src/week2/MandelbrotQuadrant.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mandelbrot;
+package week2;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
