e360886de29b178c2d1d264c4f6d9857914748bb
up780461 <up780461@1.capita.vpn.port.ac.uk>
2019-02-04 10:56:16 +0000

    Week 3, GoL

--- src/week2/Mandelbrot.java
+++ src/week2/Mandelbrot.java
@@ -5,85 +5,78 @@
  */
 package week2;
 
-import java.awt.Color ;
-import java.awt.image.BufferedImage ;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 
-import java.io.File ;
+import java.io.File;
 
 public class Mandelbrot {
 
-    final static int N = 4096 ;
-    final static int CUTOFF = 100 ;
+    final static int N = 4096;
+    final static int CUTOFF = 100;
 
-    static int [] [] set = new int [N] [N] ;
+    static int[][] set = new int[N][N];
 
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
         // Calculate set
-
         long startTime = System.currentTimeMillis();
 
         // main calculation loop
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 
-        for(int i = 0 ; i < N ; i++) {
-            for(int j = 0 ; j < N ; j++) {
+                double cr = (4.0 * i - 2 * N) / N;
+                double ci = (4.0 * j - 2 * N) / N;
 
-                  double cr = (4.0 * i - 2 * N) / N ;
-                  double ci = (4.0 * j - 2 * N) / N ;
+                double zr = cr, zi = ci;
 
-                  double zr = cr, zi = ci ;
+                int k = 0;
+                while (k < CUTOFF && zr * zr + zi * zi < 4.0) {
 
-                  int k = 0 ;
-                  while (k < CUTOFF && zr * zr + zi * zi < 4.0) {
+                    // z = c + z * z
+                    double newr = cr + zr * zr - zi * zi;
+                    double newi = ci + 2 * zr * zi;
 
-                      // z = c + z * z
+                    zr = newr;
+                    zi = newi;
 
-                      double newr = cr + zr * zr - zi * zi ;
-                      double newi = ci + 2 * zr * zi ;
+                    k++;
+                }
 
-                      zr = newr ;
-                      zi = newi ;
+                set[i][j] = k;
+            }
+        }
 
-                      k++ ;
-                  }
+        long endTime = System.currentTimeMillis();
 
-                  set [i] [j] = k ;
-              }
-          }
+        System.out.println("Calculation completed in "
+                + (endTime - startTime) + " milliseconds");
 
-          long endTime = System.currentTimeMillis();
+        // Plot image
+        BufferedImage img = new BufferedImage(N, N,
+                BufferedImage.TYPE_INT_ARGB);
 
-          System.out.println("Calculation completed in " +
-                             (endTime - startTime) + " milliseconds");
+        // Draw pixels
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 
-          // Plot image
+                int k = set[i][j];
 
-          BufferedImage img = new BufferedImage(N, N,
-                                                BufferedImage.TYPE_INT_ARGB) ;
+                float level;
+                if (k < CUTOFF) {
+                    level = (float) k / CUTOFF;
+                } else {
+                    level = 0;
+                }
+                Color c = new Color(level / 2, level / 2, level);  // Blueish
+                img.setRGB(i, j, c.getRGB());
+            }
+        }
 
-          // Draw pixels
-
-          for (int i = 0 ; i < N ; i++) {
-              for (int j = 0 ; j < N ; j++) {
-
-                  int k = set [i] [j] ;
-
-                  float level ;
-                  if(k < CUTOFF) {
-                      level = (float) k / CUTOFF ;
-                  }
-                  else {
-                      level = 0 ;
-                  }
-                  Color c = new Color(level/2, level/2, level) ;  // Blueish
-                  img.setRGB(i, j, c.getRGB()) ;
-              }
-          }
-
-          // Print file
-
-          ImageIO.write(img, "PNG", new File("Mandelbrot.png"));
-      }
-  }
+        // Print file
+        ImageIO.write(img, "PNG", new File("Mandelbrot.png"));
+    }
+}
